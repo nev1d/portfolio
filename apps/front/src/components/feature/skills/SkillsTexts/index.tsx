805aac5d259a68b4aff2402da2b1b87998de44b2
Daniil Nikonyuk <daniil.nikonyuk@gmail.com>
2024-07-03 01:20:00 +0200

    fix(text): change texts

--- apps/front/src/components/feature/skills/SkillsTexts/index.tsx
+++ apps/front/src/components/feature/skills/SkillsTexts/index.tsx
@@ -25,7 +25,7 @@ const skillsTexts: SkillsTextsItem[] = [
         title: 'React Ecosystem',
         position: [150, 140],
         description:
-            'The place where I feel most comfortable. In addition to architecting huge high-load projects, I have experience as a Team Lead for teams ranging from 4 to 10 developers on these projects. I have extensive experience with server-side rendering (SSR) and its custom implementation in every popular framework, as well as building headless CMS. I also have used various tech stacks from React 15 up to the present day',
+            'This is where I thrive. Beyond architecting large-scale, high-load projects, I specialize in server-side rendering (SSR) with custom implementations across major frameworks and building headless CMS solutions. I bring extensive experience across the React ecosystem, adept with various tech stacks from React 15 to the latest advancements.',
         tags: ['Typescript', 'React', 'Remix', 'NextJS', 'Redux', 'Redux Toolkit', 'Mobx', 'Effector', 'React Query'],
         progress: 100,
     },
@@ -34,7 +34,7 @@ const skillsTexts: SkillsTextsItem[] = [
         title: 'Vue Ecosystem',
         position: [140, 120],
         description:
-            'Vue is also a thing, that i like. Having not gotten along in the first days, I am happy to write projects on Vue today. I caught the transition from Vue 2 to Vue 3 and took part in rewriting it on one large project. In addition to that i have big experience with Nuxt 2 and 3 versions. I built multiple pet-projects by myself and participated in large projects as i mentioned before',
+            "Vue has become a passion of mine. Though we didn't click right away, today I thoroughly enjoy developing projects with Vue. I actively participated in the transition from Vue 2 to Vue 3, playing a key role in rewriting a significant project. I have extensive experience with both Nuxt 2 and 3 versions, having built numerous pet-projects independently and contributed to large-scale projects, as mentioned earlier.",
         tags: ['Typescript', 'Vue 2', 'Vue 3', 'Vuex', 'Pinia', 'NuxtJS', 'Vue Query'],
         progress: 100,
     },
@@ -43,7 +43,7 @@ const skillsTexts: SkillsTextsItem[] = [
         title: 'FrontOps',
         position: [120, 100],
         description:
-            'The world of FrontOps turned out to be a big discovery for me. I find very relaxing to make a project not only good from visual side, but also work faster, and most important convenient for developers. I think many companies underestimating a part of FrontOps. It is the place, where you can speed up all processes, from developer work time, to deploying. And despite the fact that this work is often invisible to business, I still love learning something new and progress in FrontOps',
+            "Discovering FrontOps has been a revelation for me. I find immense satisfaction in optimizing projects not only for visual appeal but also for enhanced speed and developer convenience. FrontOps, often overlooked by many companies, accelerates processes—from developer efficiency to deployment speed. Despite its behind-the-scenes nature in business, I'm passionate about continual learning and advancing in FrontOps.",
         tags: ['ViteJS', 'Webpack', 'Rollup', 'Esbuild', 'Eslint', 'Git Hooks', 'Stylelint', 'Sonar'],
         progress: 100,
     },
@@ -52,7 +52,7 @@ const skillsTexts: SkillsTextsItem[] = [
         title: 'Backend',
         position: [100, 80],
         description:
-            'I am not a huge expert of Backend development, but i try my best. I dont have a big experience of writing a huge projects in backend part, but still have some cases, where i did that, mostly using NestJS + Typeorm. I understand how backend works and can give some advices in architecture, but my main task as Frontend developer - make work with backend simple, safe and comfortable for both sides. In additional to that i wrote a backend for my pet-project by myself day by day improving my skills in it',
+            "While my expertise lies primarily in Frontend development, I also venture into Backend with dedication and enthusiasm. Although I haven't accumulated extensive experience in large-scale Backend projects, I've successfully tackled several using NestJS and TypeORM. My understanding of Backend architecture allows me to provide valuable insights and advice. As a Frontend developer, my goal is to streamline and enhance the interaction between Frontend and Backend systems, ensuring simplicity, security, and seamless functionality. I continuously hone my Backend skills by independently developing and refining a backend for my pet project.",
         tags: ['NestJS', 'Prisma', 'Typeorm', 'Mongoose', 'MongoDB', 'PostgreSQL', 'Redis'],
         progress: 40,
     },
@@ -61,7 +61,7 @@ const skillsTexts: SkillsTextsItem[] = [
         title: 'DevOps',
         position: [80, 60],
         description:
-            'DevOps is also a part that i like. I have a big experience with building a high-load applications using k8s/Marathon especially for frontend services. It is not a hard part to make application work in Docker container or deploying thru the GitLab CI or Github Actions. I have room to grow and don’t forget about this important part.',
+            "I also have a strong affinity for DevOps. My experience includes building high-load applications using Kubernetes and Marathon, particularly for frontend services. I'm adept at containerizing applications with Docker and deploying them efficiently through GitLab CI or GitHub Actions. While I've achieved proficiency in these areas, I remain committed to continuous improvement and expanding my expertise in DevOps.",
         tags: ['Docker', 'Docker Compose', 'GitLab CI', 'Jenkins', 'Marathon', 'Mesos', 'Github Actions', 'k8s'],
         progress: 35,
     },
